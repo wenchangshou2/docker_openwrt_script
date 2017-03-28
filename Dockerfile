d223ba3ed66bcd8690040585a06982d8fc890926
wenchangshou <wenchangshou@live.cn>
2017-03-28 17:58:01 +0800

    2

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM ubuntu:16.04
 RUN mkdir -p /works/openwrt
-COPY init.sh /works/init.sh
 RUN sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list &&\
     apt-get update &&\
     apt-get upgrade -y &&\
     apt-get install -y wget unzip
+COPY init.sh /works/init.sh
 RUN sh /works/init.sh
--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 cd /works
-wget ngrokc.top:8000/openwrt-scripts-4c21b204c7fcfd86ec407c3c75bbfbe0a339a803.zip  &&\
-    unzip /works/openwrt-scripts-4c21b204c7fcfd86ec407c3c75bbfbe0a339a803.zip /works/openwrt &&\
-    cd /works/openwrt && ./install_openwrt_hostlib.sh
+wget ngrokc.top:8000/openwrt-scripts-4c21b204c7fcfd86ec407c3c75bbfbe0a339a803.zip  
+unzip /works/openwrt-scripts-4c21b204c7fcfd86ec407c3c75bbfbe0a339a803.zip  
+mv /works/openwrt-scripts.git /works/openwrt3
+cd /works/openwrt3 && /works/openwrt3/install_openwrt_hostlib.sh 
 
